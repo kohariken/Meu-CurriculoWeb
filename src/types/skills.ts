a35371b15d2f82c97526acb9711b958ec4efed9e
kohariken <104532358+kohariken@users.noreply.github.com>
2024-08-16 04:40:43 -0300

    ajustes

--- src/types/skills.ts
+++ src/types/skills.ts
@@ -15,35 +15,35 @@ export const skillsData: Skill[] = [
     name: 'MongoDB',
     icon: SiMongodb,
     description: 'Banco de dados NoSQL flexível e escalável',
-    level: 80,
-    endorsements: 50
+    level: 60,
+    endorsements: 30
   },
   {
     name: 'Node.js',
     icon: FaNodeJs,
     description: 'Ambiente de execução JavaScript do lado do servidor',
-    level: 85,
-    endorsements: 60
+    level: 70,
+    endorsements: 30
   },
   {
     name: 'JavaScript',
     icon: SiJavascript,
     description: 'Linguagem de programação versátil para web',
     level: 90,
-    endorsements: 70
+    endorsements: 50
   },
   {
     name: 'Express',
     icon: SiExpress,
     description: 'Framework web rápido e minimalista para Node.js',
-    level: 75,
-    endorsements: 45
+    level: 50,
+    endorsements: 20
   },
   {
     name: 'React',
     icon: FaReact,
     description: 'Biblioteca JavaScript para construir interfaces de usuário',
-    level: 85,
-    endorsements: 55
+    level: 40,
+    endorsements: 10
   }
 ];
